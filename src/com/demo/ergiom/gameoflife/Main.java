6c2c7d26f79132745ef90364b2d2e215d2ce302c
ergiom <ergiom1@gmail.com>
2022-12-21 16:56:50 +0100

    changed int to final form

--- src/com/demo/ergiom/gameoflife/Main.java
+++ src/com/demo/ergiom/gameoflife/Main.java
@@ -1,34 +1,10 @@
 package com.demo.ergiom.gameoflife;
 
-import com.demo.ergiom.gameoflife.game.Game;
-import com.demo.ergiom.gameoflife.game.grid.Position;
+import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) {
-        Game game = Game.wrappedBordersGame(10, 10);
-
-        game.setAlive(new Position(4, 1));
-        game.setAlive(new Position(4, 2));
-        game.setAlive(new Position(5, 0));
-        game.setAlive(new Position(5, 1));
-        game.setAlive(new Position(6, 1));
-
-        System.out.println(game);
-        System.out.println();
-
-        game.playRound();
-
-        System.out.println(game);
-        System.out.println();
-
-        game.playRound();
-
-        System.out.println(game);
-        System.out.println();
-
-        game.playRound();
-
-        System.out.println(game);
-        System.out.println();
+    public static void main(String[] args) throws IOException {
+        Controller controller = new Controller();
+        controller.loop();
     }
 }
